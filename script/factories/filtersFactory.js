9bdaa3545d84ebd2ae13e5300aa328f3c9e7e12e
DESKTOP-H3VHUFJ\Manon <manon.a.dauguet@orange.fr>
2023-08-24 09:20:15 +0200

    refactor majeur: création d'une classe FiltreComponent. La gestion des 3 composants de filtres fait appelle à leurs constructeurs et leurs méthodes, automatisant la gestion de leurs paramètres distinctifs.

---
+++ script/factories/filtersFactory.js
@@ -0,0 +1,85 @@
+import firstUppercase from "../utils.js";
+
+
+class FilterComponent {
+    constructor(filterId, filterSubject, searchInput, openButtonFilter, optionsContainerContainer) {
+        this._id = filterId;
+        this._subject = filterSubject;
+        this._searchInput = searchInput;
+        this._openButton = openButtonFilter;
+        this._optionsContainer = optionsContainerContainer;
+    }
+
+
+    getAllOptions(array) {
+        if (this._id == 1) {
+            return this.getAllIngredients(array);
+        } else if (this._id == 2) {
+            return this.getAllAppliance(array);
+        } else if (this._id == 3) {
+            return this.getAllUstensils(array);
+        }
+    }  
+
+    getFilterOptionDOM(option) {
+        const optionDOM = document.createElement( 'li' );
+        optionDOM.classList.add( 'list-group-item' );
+        optionDOM.innerText = option;
+        return optionDOM;
+    }
+
+
+    // Récupération des options selon les recettes disponibles et le filtre concerné
+    getAllIngredients(array) {
+        let allIngredients = [];
+        for (let i = 0; i < array.length; i++) {
+            try {
+                const ingredients = array[i].ingredients;
+                for (let j = 0; j < ingredients.length; j++) {
+                    let ingredient = firstUppercase(ingredients[j].ingredient);
+                    if (!allIngredients.includes(ingredient)) {
+                        allIngredients.push(ingredient);
+                    }
+                }                
+            } catch (error) {
+                allIngredients = Array.from(array);
+            }
+        }
+        return allIngredients.sort();
+    }
+
+    getAllAppliance(array) {
+        let allAppliance = [];
+        for (let i = 0; i < array.length; i++) {
+            try {
+                let appliance = firstUppercase(array[i].appliance);
+                if (!allAppliance.includes(appliance)) {
+                    allAppliance.push(appliance);
+                }                
+            } catch (error) {
+                allAppliance = Array.from(array);
+            }
+        }
+        return allAppliance.sort();
+    }
+    
+    getAllUstensils(array) {
+        let allUstensils = [];
+        for (let i = 0; i < array.length; i++) {
+            try {
+                const ustensils = array[i].ustensils;
+                for (let j = 0; j < ustensils.length; j++) {
+                    let ustensil = firstUppercase(ustensils[j]);
+                    if (!allUstensils.includes(ustensil)) {
+                        allUstensils.push(ustensil);
+                    }
+                }                
+            } catch (error) {
+                allUstensils = Array.from(array);
+            }
+        }
+        return allUstensils.sort();
+    }
+}
+
+export default FilterComponent;
--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@
 import firstUppercase from "./utils.js";
 import Recipes from "./factories/recipesFactory.js";
 import Tag from "./factories/tagsFactory.js";
-import FilterOption from "./factories/filtersOptionsFactory.js";
+import FilterComponent from "./factories/filtersFactory.js";
 import recipes from "../recipes.js";
 
 
@@ -10,20 +10,24 @@ import recipes from "../recipes.js";
 const recipesContainer = document.querySelector( '.recipes_container' );
 const optionsSelectedContainer = document.querySelector( '.options_selected_container' );
 
+const form0 = document.querySelector( 'form[name="form_search"]');
+const form1 = document.querySelector( 'form[name="form_search1"]');
+const form2 = document.querySelector( 'form[name="form_search2"]');
+const form3 = document.querySelector( 'form[name="form_search3"]');
+const forms = [form0, form1, form2, form3];
+
 const searchMain = document.getElementById( 'search' );
 const searchFilter1 = document.getElementById( 'search1' );
 const searchFilter2 = document.getElementById( 'search2' );
 const searchFilter3 = document.getElementById( 'search3' );
-const searchInputs = [searchMain, searchFilter1, searchFilter2, searchFilter3];
 
 const openButtonFilter1 = document.getElementById( 'filter1-checkbox' );
 const openButtonFilter2 = document.getElementById( 'filter2-checkbox' );
 const openButtonFilter3 = document.getElementById( 'filter3-checkbox' );
-const openButtonFilter = [openButtonFilter1, openButtonFilter2, openButtonFilter3];
 
-const ingredientsOptions = document.querySelector( '#listbox1 .list_options' );
-const appliancesOptions = document.querySelector( '#listbox2 .list_options' );
-const ustensilsOptions = document.querySelector( '#listbox3 .list_options' );
+const ingredientsOptionsContainer = document.querySelector( '#listbox1 .list_options' );
+const appliancesOptionsContainer = document.querySelector( '#listbox2 .list_options' );
+const ustensilsOptionsContainer = document.querySelector( '#listbox3 .list_options' );
 
 
 // Création des cartes recettes (fonction)
@@ -56,138 +60,100 @@ function addTag(optionSelected) {
 }
 
 
+
 /**************** INITIALISATION DE LA PAGE ********************/
 // Affichage dynamique des cartes recettes
 displayRecipes(recipes);
+let filteredRecipes = Array.from(recipes);
 
-// Remplissage dynamique des filtres
-let allIngredients = getAllIngredients(recipes);
-let allAppliance = getAllAppliance(recipes);
-let allUstensils = getAllUstensils(recipes);
-displayOptionsFilter(allIngredients, 1);
-displayOptionsFilter(allAppliance, 2);
-displayOptionsFilter(allUstensils, 3);
+
+// Création des objets filtres
+let filterComponent1 = new FilterComponent(1, "ingredients", searchFilter1, openButtonFilter1, ingredientsOptionsContainer);
+let filterComponent2 = new FilterComponent(2, "appliances", searchFilter2, openButtonFilter2, appliancesOptionsContainer);
+let filterComponent3 = new FilterComponent(3, "ustensils", searchFilter3, openButtonFilter3, ustensilsOptionsContainer);
+let filterComponents = [filterComponent1, filterComponent2, filterComponent3];
+
+
+// Affichage dynamique des filtres
+for (let i = 0; i < filterComponents.length; i++) {
+    displayOptionsFilter(filterComponents[i], recipes);
+}
 
 
 
 /******************* GESTION DES FILTRES **********************/
 // Comportement d'affichage des filtres
-for (let i = 0; i < openButtonFilter.length; i++) {
-    openButtonFilter[i].addEventListener("change", () => {
-        if (openButtonFilter[i].checked) {
-            document.querySelector( `label[for="${openButtonFilter[i].id}"] .chevron`).setAttribute("alt", "refermer");
+for (let i = 0; i < filterComponents.length; i++) {
+    filterComponents[i]._openButton.addEventListener("change", () => {
+        if (filterComponents[i]._openButton.checked) {
+            document.querySelector( `label[for="${filterComponents[i]._openButton.id}"] .chevron`).setAttribute("alt", "refermer");
         }        
-        if (!openButtonFilter[i].checked) {
-            document.querySelector( `label[for="${openButtonFilter[i].id}"] .chevron`).setAttribute("alt", "etendre");
+        else {
+            document.querySelector( `label[for="${filterComponents[i]._openButton.id}"] .chevron`).setAttribute("alt", "etendre");
         }  
     })
 }
 
+
 // Remplissage dynamique des filtres (fonctions)
-function displayOptionsFilter(array, filterNumber) {
-    for (let i = 0; i < array.length; i++) {
-        const option = new FilterOption(array[i]).getFilterOptionDOM();
-        switch (filterNumber) {
-            case 1:
-                option.setAttribute("id", `listbox1-${i}`);
-                ingredientsOptions.appendChild(option);
-                break;
-            case 2:
-                option.setAttribute("id", `listbox2-${i}`);
-                appliancesOptions.appendChild(option);
-                break;
-            case 3:
-                option.setAttribute("id", `listbox3-${i}`);
-                ustensilsOptions.appendChild(option);
-                break;
-        }
+function displayOptionsFilter(filterComponentObject, array) {
+    filterComponentObject._optionsContainer.innerHTML = "";
+    const allOptions = filterComponentObject.getAllOptions(array);
+
+    for (let i = 0; i < allOptions.length; i++) {
+        const option = filterComponentObject.getFilterOptionDOM(allOptions[i]);
+        filterComponentObject._optionsContainer.appendChild(option);
         option.addEventListener("click", () => {
             addTag(option);
-            openButtonFilter[filterNumber-1].checked = false;
+            filterComponentObject._openButton.checked = false;
             filterRecipesWithTags();
+            console.log(`filtrer par "${option.innerText}"`);
         })
     }   
-}
-
-function getAllIngredients(array) {
-    let allIngredients = [];
-    for (let i = 0; i < array.length; i++) {
-        const ingredients = array[i].ingredients;
-        for (let j = 0; j < ingredients.length; j++) {
-            let ingredient = firstUppercase(ingredients[j].ingredient);
-            if (!allIngredients.includes(ingredient)) {
-                allIngredients.push(ingredient);
-            }
-        }
-    }
-    return allIngredients.sort();
-}
-
-function getAllAppliance(array) {
-    let allAppliance = [];
-    for (let i = 0; i < array.length; i++) {
-        let appliance = firstUppercase(array[i].appliance);
-        if (!allAppliance.includes(appliance)) {
-            allAppliance.push(appliance);
-        }
-    }
-    return allAppliance.sort();
-}
-
-function getAllUstensils(array) {
-    let allUstensils = [];
-    for (let i = 0; i < array.length; i++) {
-        const ustensils = array[i].ustensils;
-        for (let j = 0; j < ustensils.length; j++) {
-            let ustensil = firstUppercase(ustensils[j]);
-            if (!allUstensils.includes(ustensil)) {
-                allUstensils.push(ustensil);
-            }
-        }
-    }
-    return allUstensils.sort();
-}
-
-// Mise à jour des filtres selon les recettes disponibles (fonction)
-function filterOptionsAvailable(array) {
-    ingredientsOptions.innerHTML = "";
-    allIngredients = getAllIngredients(array);
-    displayOptionsFilter(allIngredients, 1);
-    appliancesOptions.innerHTML = "";
-    allAppliance = getAllAppliance(array);
-    displayOptionsFilter(allAppliance, 2);
-    ustensilsOptions.innerHTML = "";
-    allUstensils = getAllUstensils(array);
-    displayOptionsFilter(allUstensils, 3);    
 }
 
 
 
 /*************** GESTION DES BARRES DE RECHERCHE ****************/
-for (let i = 0; i < searchInputs.length; i++) {
-    searchInputs[i].addEventListener("submit", (event) => {
-        event.preventDefault();
-    })
-    searchInputs[i].addEventListener("keydown", () => { 
+// Gestion de la barre principale de recherche
+form0.addEventListener("submit", (event) => {
+    event.preventDefault();
+})  
+
+searchMain.addEventListener("keydown", () => { 
     // appliquer à d'autres évennements, comme focus, onpaste...
-        if (searchInputs[i].value.trim().length+1 >= 3) {
-            filterRecipesWithSearch(); // algorithme de recherche
-        }
-        openEraseButton(searchInputs[i]);
+    if (searchMain.value.trim().length+1 >= 3) {
+        filterRecipesWithSearch(); // algorithme de recherche
+    }
+    openEraseButton(searchMain);
+});
+
+
+// Gestion des barres de recherches associées aux objects filtres
+for (let i = 0; i < filterComponents.length; i++) {
+    let input = filterComponents[i]._searchInput;
+    let form = forms[i+1];
+
+    input.addEventListener("keydown", () => {
+        console.log(input.value);
+        filterOptionsWithSearch(filterComponents[i], input.value);
+        openEraseButton(input, filterComponents[i]);
+    })
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        if (filterComponents[i]._optionsContainer.firstChild) {
+            console.log(filterComponents[i]._optionsContainer.firstChild)
+            addTag(filterComponents[i]._optionsContainer.firstChild);
+            filterComponents[i]._openButton.checked = false;
+            filterRecipesWithTags();
+        }        
     })
 }
 
-// Selection rapide d'un tag parmis les options disponibles
-[searchFilter1, searchFilter2, searchFilter3].forEach((input) => {
-    input.addEventListener("keydown", (event) => {
-        console.log(input.value + event.key);
-        filterOptionsWithSearch(input, (input.value + event.key));
-    })
-})
-
 // Ajout d'un bouton "effacer" (fonction)
-function openEraseButton(input) {
-    const eraseButton = document.querySelector( `.form_${input.id} .close_button`);
+function openEraseButton(input, filterComponentObject = undefined) {
+    const eraseButton = document.querySelector( `form[name="form_${input.id}"] .close_button`);
     if (input.value.length == null || input.value.length == "") {
         eraseButton.style.display = "none";
     } else {
@@ -196,6 +162,9 @@ function openEraseButton(input) {
     eraseButton.addEventListener("click", () => {
         input.value = null;
         eraseButton.style.display = "none";
+        if (filterComponentObject != undefined) {
+            displayOptionsFilter(filterComponentObject, filteredRecipes);
+        }
     })
 }
 
@@ -203,67 +172,50 @@ function openEraseButton(input) {
 
 /*************** ALGORITHMES DE RECHERCHE ****************/
 function filterRecipesWithTags() {
-// Filtre les recettes selon les tags sélectionnés
-// Appelée lors de l'jaout de la suppression d'un tag
-// S'il n'y aucune recette qui correspond, alors affichage du messsage d'erreur    
+    // Filtre les recettes selon les tags sélectionnés
+    // Appelée lors de l'ajout ou de la suppression d'un tag
+    // S'il n'y aucune recette qui correspond, alors affichage du messsage d'erreur    
     const allTags = document.querySelectorAll( '.option_selected');
     for (let i = 0; i < allTags.length; i++) {
         // console.log(allTags[i].innerText);
     }
 
-    // test
     if (allTags.length != 0) {
-        let filteredRecipes = recipes.filter((recipes) => {
-            return recipes.id <= 5; // algorithme de recherche
+        filteredRecipes = filteredRecipes.filter((recipe) => {
+            return recipe.id <= 5; // algorithme de recherche
         })
         displayRecipes(filteredRecipes);
-        filterOptionsAvailable(filteredRecipes);        
+        for (let i = 0; i < filterComponents.length; i++) {
+            displayOptionsFilter(filterComponents[i], filteredRecipes)  
+        }
     } else {
+        filteredRecipes = Array.from(recipes);
         displayRecipes(recipes);
-        filterOptionsAvailable(recipes);
+        for (let i = 0; i < filterComponents.length; i++) {
+            displayOptionsFilter(filterComponents[i], recipes)   
+        }
     }
-
 }
 
 
 function filterRecipesWithSearch(inputValue) {
-// Filtre les recettes selon la recherche effectuée dans la barre principale
-// Appelée par un event sur la barre principale
-// S'il n'y aucune recette qui correspond, alors affichage du messsage d'erreur    
+    // Filtre les recettes selon la recherche effectuée dans la barre principale
+    // Appelée par un event sur la barre principale
+    // S'il n'y aucune recette qui correspond, alors affichage du messsage d'erreur   
 
 }
 
 
-function filterOptionsWithSearch(input, inputValue) {
+function filterOptionsWithSearch(filterComponentObject, inputValue) {
     // Filtre les options qui correspondent aux entrées de l'utilisateur dans chaque filtre
     // Appelée par un event sur la barre de recherche du filtre associé
-        let optionsFiltered = [];
-        let optionsAvailable;
-        let optionsAvailableContener;
-        let filterNumber;
-        switch (input) {
-            case searchFilter1:
-                optionsAvailable = allIngredients;
-                optionsAvailableContener = ingredientsOptions;
-                filterNumber = 1;
-                break;
-            case searchFilter2:
-                optionsAvailable = allAppliance;
-                optionsAvailableContener = appliancesOptions;
-                filterNumber = 2;
-                break;
-            case searchFilter3:
-                optionsAvailable = allUstensils;
-                optionsAvailableContener = ustensilsOptions;
-                filterNumber = 3;
-                break;
-        }
+    let optionsFiltered = [];
+    let optionsAvailable = filterComponentObject.getAllOptions(filteredRecipes);
     
-        for (let i = 0; i < optionsAvailable.length; i++) {
-            if (optionsAvailable[i].includes((inputValue))) {
-                optionsFiltered.push(optionsAvailable[i]);
-            }
+    for (let i = 0; i < optionsAvailable.length; i++) {
+        if (optionsAvailable[i].includes((inputValue))) {
+            optionsFiltered.push(optionsAvailable[i]);
         }
-        optionsAvailableContener.innerHTML = "";
-        displayOptionsFilter(optionsFiltered, filterNumber);
-    }
+    }  
+    displayOptionsFilter(filterComponentObject, optionsFiltered);
+}
